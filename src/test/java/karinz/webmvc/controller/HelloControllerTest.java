9f7f985c6198d5349a5ca20fbc2fdbda9cafa69c
nilam <nilammartaanggreni@gmail.com>
2025-08-14 12:47:20 +0800

    add mock MVC

---
+++ src/test/java/karinz/webmvc/controller/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package karinz.webmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
